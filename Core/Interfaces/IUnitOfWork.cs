d2b1da54a736ea6d6f0707634307c1b9c1363eb6
Abdullah Ragab <abdullah.ragab2799@gmail.com>
2025-12-19 19:16:36 +0200

    z

--- Core/Interfaces/IUnitOfWork.cs
+++
@@ -1,7 +0,0 @@
-namespace Agentic_Rentify.Core.Interfaces;
-
-public interface IUnitOfWork : IDisposable
-{
-    IGenericRepository<T> Repository<T>() where T : BaseEntity;
-    Task<int> CompleteAsync();
-}
